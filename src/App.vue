5e53115cb7e7465c7cf33aae45c0f1e8e7b811d2
arizona-08 <assijonathan2@gmail.com>
2025-06-26 22:25:25 +0200

    Initialisation du projet

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <!-- Regarder dans le fichier router/index.ts pour voir la définition des routes -->
+  <!-- Regarder dans le dossier views pour voir les composants pour chaque route -->
+
+  <!-- <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header> -->
+  <RouterView />
+</template>
